bb3ef6a5d7af066256fc2388c00ddc025d3b8adf
Baharudin78 <bencoxgame78@gmail.com>
2021-01-25 21:55:55 +0700

    Initial commit

---
+++ app/src/main/java/com/baharudin/runningapp/db/Run.kt
@@ -0,0 +1,18 @@
+package com.baharudin.runningapp.db
+
+import android.graphics.Bitmap
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "running_table")
+data class Run(
+    var img : Bitmap? = null,
+    var timeStamp : Long = 0L,
+    var avgSpeedKMH : Float = 0F,
+    var distanceInMeter : Int = 0,
+    var timeInMilis : Long = 0L,
+    var caloriesBurned : Int = 0
+) {
+    @PrimaryKey(autoGenerate = true)
+    var id : Int ? = null
+}
