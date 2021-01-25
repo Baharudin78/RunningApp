2b68d2c8b7dda5a5312673251c383cf732800a77
Baharudin78 <bencoxgame78@gmail.com>
2021-01-25 22:13:27 +0700

    add Database and type converter

---
+++ app/src/main/java/com/baharudin/runningapp/db/RunningDatabases.kt
@@ -0,0 +1,14 @@
+package com.baharudin.runningapp.db
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+
+@Database(
+    entities = [Run::class],
+    version = 1
+)
+@TypeConverters(Converters::class)
+abstract class RunningDatabases : RoomDatabase() {
+    abstract fun getRunDao () : RunDAO
+}
