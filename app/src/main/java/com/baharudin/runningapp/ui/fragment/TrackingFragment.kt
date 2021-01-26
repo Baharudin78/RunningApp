dec77b4e2c65f2e524b69f399ceb78bdc1efc338
Baharudin78 <bencoxgame78@gmail.com>
2021-01-26 22:43:33 +0700

    google map handling lifecycle

--- app/src/main/java/com/baharudin/runningapp/ui/fragment/TrackingFragment.kt
+++ app/src/main/java/com/baharudin/runningapp/ui/fragment/TrackingFragment.kt
@@ -1,12 +1,62 @@
 package com.baharudin.runningapp.ui.fragment
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.baharudin.runningapp.R
+import com.baharudin.runningapp.databinding.FragmentTrackingBinding
 import com.baharudin.runningapp.ui.viewModel.MainViewModel
+import com.google.android.gms.maps.GoogleMap
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class TrackingFragment : Fragment(R.layout.fragment_tracking) {
+
+    private var _binding : FragmentTrackingBinding? = null
+    private val binding get() = _binding!!
     private val viewModel : MainViewModel by viewModels()
+    private var map : GoogleMap? = null
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        _binding = FragmentTrackingBinding.bind(view)
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.mapView.onCreate(savedInstanceState)
+
+        binding.mapView.getMapAsync{
+            map = it
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding.mapView?.onResume()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        binding.mapView?.onStart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        binding.mapView?.onStop()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        binding.mapView?.onPause()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        binding.mapView?.onLowMemory()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        binding.mapView?.onSaveInstanceState(outState)
+    }
+
 }
